82e208ce77d19260437fccacf5501a411fecf1db
Wanlu D <46799027+WanluD@users.noreply.github.com>
2023-09-09 12:24:48 -0400

    login and signup completed

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -17,94 +17,91 @@ import bcrypt from 'bcryptjs';
 */
 
 const userController = {
-    
-    // Creating a new user
-    createUser: async (
-      req: Request, 
-      res: Response, 
-      next: NextFunction,
-      ): Promise<void> => {
-        const { username, password } = req.body;
-        // checking if input fields are empty
-        if (!username || !password) {
-          return next({
-            log: "Error in userController.createUser. Missing input fields",
-            status: 400,
-            message: { err: "All fields required" },
-          });
-        }
-      
-        try {
-          // Check if the username already exists
-          const existingUserQuery = `SELECT * FROM "users" WHERE username = $1`;
-          const existingUserValues = [req.body.username];
-          const existingUserResult = await pool.query(
-             existingUserQuery,
-             existingUserValues
-          );
-          if (existingUserResult.rows.length > 0) {
-            res.status(409).json({ error: 'Username already exists' });
-            return next()
-          }
 
-          // create user in database
-          const salt = await bcrypt.genSalt(10);
-          const hashedPassword = await bcrypt.hash(password, salt);
-          const insertQuery = `INSERT INTO "users" (username, password) VALUES ($1, $2) RETURNING *`;
-          const insertValues = [username, hashedPassword];
-          const createUser = await pool.query(insertQuery, insertValues);
-          //save the user's id to res.locals
-          res.locals.userId = createUser.rows[0].user_id;
-          return next();
-        } catch (err) {
-          return next({
-            log: `Error occurred in createUser-create ${err}`,
-            status: 500,
-            message: { err: "Unable to create user" },
-          });
-        }
-      },
+  // Creating a new user
+  createUser: async (
+    req: Request, res: Response, next: NextFunction
+  ): Promise<void> => {
+    const { username, password } = req.body;
+    // checking if input fields are empty
+    if (!username || !password) {
+      return next({
+        log: "Error in userController.createUser. Missing input fields",
+        status: 400,
+        message: { err: "All fields required" },
+      });
+    }
 
-      // verify user
-      verifyUser: async (req: Request, res: Response, next: NextFunction): Promise<void> => {       
-        const { username, password } = req.body;
+    try {
+      // Check if the username already exists
+      const existingUserQuery = `SELECT * FROM "users" WHERE username = $1`;
+      const existingUserValues = [req.body.username];
+      const existingUserResult = await pool.query(
+        existingUserQuery,
+        existingUserValues
+      );
+      if (existingUserResult.rows.length > 0) {
+        res.status(409).json({ error: 'Username already exists' });
+        return next()
+      }
 
-        // Check for missing input fields
-        if (!username || !password) {
-          return next({
-            log: "Error in userController.verifyUser. Missing input fields",
-            status: 400,
-            message: { err: "All fields required" },
-          });
-        }
-      
-        try {
-          // Check if the username exists
-          const existingUsernameQuery = `SELECT * FROM "users" WHERE username = $1`;
-          const userResult = await pool.query(existingUsernameQuery, [req.body.username]);
-      
-          // If no user found by that username
-          if (userResult.rows.length === 0) {
-            res.status(409).json({ error: 'Invalid Username or Password' });
-          }
+      // create user in database
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const insertQuery = `INSERT INTO "users" (username, password) VALUES ($1, $2) RETURNING *`;
+      const insertValues = [username, hashedPassword];
+      const createUser = await pool.query(insertQuery, insertValues);
+      //save the user's id to res.locals
+      res.locals.userId = createUser.rows[0].user_id;
+      return next();
+    } catch (err) {
+      return next({
+        log: `Error occurred in createUser-create ${err}`,
+        status: 500,
+        message: { err: "Unable to create user" },
+      });
+    }
+  },
 
-          // check if password is correct
-          const user = userResult.rows[0];
-          const isPasswordMatch = await bcrypt.compare(password, user.password);
-          if (!isPasswordMatch) {
-            res.status(401).json({ error: 'Invalid Username or Password' });
-          }
-          res.locals.user = user.id;
-          return next()
-        } catch (err) {
-          return next({
-            log: `Error occurred in userController.verifyUser ${err}`,
-            status: 500,
-            message: { err: "Unable to verify user" },
-          });
-        }
-      },
-      
+  // verify user
+  verifyUser: async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { username, password } = req.body;
+
+    // Check for missing input fields
+    if (!username || !password) {
+      return next({
+        log: "Error in userController.verifyUser. Missing input fields",
+        status: 400,
+        message: { err: "All fields required" },
+      });
+    }
+
+    try {
+      // Check if the username exists
+      const existingUsernameQuery = `SELECT * FROM "users" WHERE username = $1`;
+      const userResult = await pool.query(existingUsernameQuery, [req.body.username]);
+
+      // If no user found by that username
+      if (userResult.rows.length === 0) {
+        res.status(409).json({ error: 'Invalid Username or Password' });
+      }
+
+      // check if password is correct
+      const user = userResult.rows[0];
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        res.status(401).json({ error: 'Invalid Username or Password' });
+      }
+      res.locals.user = user.id;
+      return next()
+    } catch (err) {
+      return next({
+        log: `Error occurred in userController.verifyUser ${err}`,
+        status: 500,
+        message: { err: "Unable to verify user" },
+      });
+    }
+  },
 }
 
 export { userController };
